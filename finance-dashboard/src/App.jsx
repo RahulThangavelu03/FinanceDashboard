b397e1a3e36ae1c646d6e1cb5d4e1a17a0e1e1ae
RahulThangavelu03 <rahulthangavelu@gmail.com>
2026-04-03 01:12:56 +0530

    Project Setup and few components

---
+++ finance-dashboard/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import{BrowserRouter,Routes,Route} from "react-router-dom"
+import Navbar from './Components/NavBar'
+
+function App() {
+
+  return (
+
+    <div>
+
+<Navbar></Navbar>
+
+
+
+
+
+    </div>
+
+
+
+
+
+
+  )
+}
+
+export default App
---
+++ finance-dashboard/src/Components/NavBar.jsx
@@ -0,0 +1,20 @@
+export default function Navbar() {
+
+  return (
+
+    <div className="w-full h-16 border-b border-gray-200">
+
+      <div className="flex flex-row ">
+
+<div>Finance DashBoard</div>
+
+<div>Role</div>
+<div>Mode</div>
+
+
+      </div>
+
+    </div>
+
+  )
+}
